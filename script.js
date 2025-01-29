40b19277d8e48a7a567b91908206833cc1d8ab27
Jacob Bilby <158042318+jbprojects95@users.noreply.github.com>
2025-01-29 10:25:11 +0000

    added book object in script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} --- ${author} --- ${pages} pages --- ${
+      read.charAt(0).toUpperCase() + read.slice(1)
+    }`;
+  };
+}
 
+// const book1 = new Book("The Hobbit", "J.R.R Tolkien", "295", "read");
+
+// console.log(book1.info());
