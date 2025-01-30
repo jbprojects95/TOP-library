784bb846a7ff8c88961ebfd33de992eaab084ed2
Jacob Bilby <158042318+jbprojects95@users.noreply.github.com>
2025-01-30 14:14:58 +0000

    addBookToLibrary function fleshed out

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-function Book(title, author, pages, read) {
+const myLibrary = [];
+
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  // this.read = "false"
+  this.read = false;
+  // **Below doesn't work for boolean values (regarding if it's been read or not),
+  // **as the commented code treats it as a string:
+  // this.info = function () {
+  //   return `${this.title} --- ${this.author} --- ${this.pages} pages --- ${
+  //     this.read.charAt(0).toUpperCase() + this.read.slice(1)
+  //   }`;
+
+  // **This uses ternary operators to check the value of this.read
+  // **If true then it returns "Read", else returns "Unread"
   this.info = function () {
-    return `${this.title} --- ${author} --- ${pages} pages --- ${
-      read.charAt(0).toUpperCase() + read.slice(1)
+    return `${this.title} --- ${this.author} --- ${this.pages} pages --- ${
+      this.read ? "Read" : "Unread"
     }`;
   };
 }
 
-// const book1 = new Book("The Hobbit", "J.R.R Tolkien", "295", "read");
+function addBookToLibrary(title, author, pages) {
+  const duplicate = myLibrary.find((book) => book.title === title);
 
-// console.log(book1.info());
+  if (duplicate) {
+    console.log(`Sorry, "${title}" is already in your library.`);
+  } else {
+    myLibrary.push(new Book(title, author, pages));
+  }
+}
+
+addBookToLibrary("Harry Potter", "J.K Rowling", "500");
+addBookToLibrary("Harry Potter", "J.K Rowling", "500");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "read");
+addBookToLibrary("Alice in Wonderland", "CS Lewis", "400");
+addBookToLibrary("Alice in Wonderland", "CS Lewis", "400");
+addBookToLibrary("Alice in Wonderland", "CS Lewis", "400");
+
+console.table(myLibrary);
